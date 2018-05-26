b577538eea4f6bff92ed0496d13a66570318fa31
mzkaramat <muhammadzeeshan020@gmail.com>
2018-05-26 14:48:08 +0000

    added option for updating user status

--- WifiAuthenticator.js
+++ WifiAuthenticator.js
@@ -3,122 +3,7 @@ pragma solidity ^0.4.0;
 contract WifiAuthenticator {
     
     //initialization string for the network name
-    
-    //locking user after 5 attempts
-    //allow user to change password
-    //new password shouldn't be similar to previous one, it shoule be more than 8 characters, no repeat chartaters, one special character, and one number
-    //blacklist users
-    //allow some users to only login between 1 to 10 PM
-    //multiple timezone
-    
-    
-    string networkname = "Hello HelloWorld";
-    
-    
-    //user struct for storing and authenticating users
-    struct user {
-        address username;
-        string key;
-        uint8 failedTries;
-        uint8 successTries;
-        uint8 downlaoed_data;
-    }
-    
-    
-     address[] blacklist_users =  [0x9AB55D9559623a78Ac7F016648f9C9325A42E57c, 0xf235aa56dd96bda02acfb361e];
-    //mapping from users to addresses
-    mapping(address => user) users;
-    uint8 numberOfUsers;
-    uint8 currentUsers;
-
-    //wifi user authenticastor
-     function WifiAuthenticator(string mesage,uint8 _numusers) public {
-        networkname = mesage;
-        numberOfUsers = _numusers;
-        currentUsers = 0;
-    }
-    
-    //getter function for getting the name of network
-     function getnetworkName() public view returns (string) {
-         return networkname;
-     }
-     
-     //get current registered nunmber of users
-     function getRegisteredUser() public view returns (uint8) {
-         return currentUsers;
-     }
-     
-     /// Give $(_newuser) the right to vote on this ballot.
-    /// May only be called by $(chairperson).
-    function addUser(address _newuser) public view returns (string) {
-        
-      if (checkUserBlackListing(_newuser)) {
-        return "user is blacklisted";
-      }
-      
-        if (currentUsers > numberOfUsers) return;
-        users[_newuser].username = _newuser;
-        users[_newuser].key = "admin123";
-        users[_newuser].failedTries = 0;
-        users[_newuser].successTries = 0;
-        currentUsers = currentUsers+1;
-        
-        return "user is added successfully";
-    }
-     
-     function checkUserBlackListing(address _user) public view returns (bool){
-          for(uint i = 0; i < blacklist_users.length; i++) {
-          if (blacklist_users[i] == _user) {
-            return true;
-          }else{
-              return false;
-          }
-        }
-     }
-     
-     //get user credentials in case the user has forgot the credentials
-     function getUserCredentials(address _usercredentials) public view returns (string) {
-         return users[_usercredentials].key;
-     }
-     
-     //get user credentials in case the user has forgot the credentials
-     function updateUserPassword(address _usercredentials,string previousPassword,string newPassword) public view returns (string) {
-         if (compareStrings( users[_usercredentials].key, previousPassword) ) {
-             if(bytes(newPassword).length < 8){
-                 return "password should be longer than 8 characters";
-             }else{
-                  if (compareStrings( newPassword, previousPassword) ) {
-                      return "Previous password shouldn't be similar to old password";
-                  }else{
-                      users[_usercredentials].key = newPassword;
-                     return "successfully changed password"; 
-                  }
-                 
-             }
-             
-         }else{
-             return "Previous password don't match";
-         }
-         
-     }
-     
-     //get user credentials in case the user has forgot the credentials
-     function getFailedTries(address _usercredentials) public view returns (uint8) {
-         return users[_usercredentials].successTries;
-     }
-     
-     //get user credentials in case the user has forgot the credentials
-     function getSuccessTries(address _usercredentials) public view returns (uint8) {
-         return users[_usercredentials].failedTries;
-     }
-     pragma solidity ^0.4.0;
-
-contract WifiAuthenticator {
-    
-    //initialization string for the network name
-    
-    //locking user after 5 attempts
-    //allow some users to only login between 1 to 10 PM
+        //allow some users to only login between 1 to 10 PM
 
     
     string networkname = "Hello HelloWorld";
@@ -190,6 +75,14 @@ contract WifiAuthenticator {
      //get user credentials in case the user has forgot the credentials
      function getUserCredentials(address _usercredentials) public view returns (string) {
          return users[_usercredentials].key;
+     }
+     
+     //get user credentials in case the user has forgot the credentials
+     function updateUserStatus(address _usercredentials,bool _status) public view returns (string) {
+         
+         users[_usercredentials].status = _status;
+         return "Successfully updated user status";
+         
      }
      
      //get user credentials in case the user has forgot the credentials
